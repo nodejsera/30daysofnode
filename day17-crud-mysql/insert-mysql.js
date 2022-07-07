ed39aad8b687cf546b0faa4312226508b84a6e48
Mustafa khan <58285154+Mustafa-khann@users.noreply.github.com>
2022-07-07 20:18:53 +0500

    Update insert-mysql.js

--- day17-crud-mysql/insert-mysql.js
+++ day17-crud-mysql/insert-mysql.js
@@ -1,26 +1,22 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connect =  mysql.createPool({
-host : 'localhost',
-user : 'root',
-password: '',
-database: 'test'
+var connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'mysql'
 });
 
+var insert_R = "INSERT INTO customers (name, address ,age) VALUES (?,?,?)";
 
-var insert_R = 'INSERT INTO details(name,age) VALUE(?,?)';
-//establishing connection
-connect.getConnection(function(err, connection){
-    
-  //Inserting a record into details
-   connection.query(insert_R,['rejii',24], function(err,res){
-    if(err) throw err;
-    else {
-        console.log('Details added successfully');
+connect.connect();
+    connect.query(insert_R,['mustafa','peshawar', 20], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        else 
+        {
+        console.log('Record Inserted Successfully');
     }
-  });
-
-//releasing connection
- connection.release();
-
-});
+    });
+connect.end();
