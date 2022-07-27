d2ff49a62681a42a2f9c878612908a9e238a7433
APNeto <penaneto@gmail.com>
2022-07-27 09:27:11 -0300

    Update find.js: the code was using the filter method instead
    Simply changed the method called upon the array from "filter" to "find".

--- day6-array-methods/find.js
+++ day6-array-methods/find.js
@@ -8,7 +8,7 @@ function odd(value){
     }
 }
 
-var out = arr.filter(odd);
-var out1 = arr1.filter(odd);
+var out = arr.find(odd);
+var out1 = arr1.find(odd);
 console.log("Output of array 1 : " + out);
 console.log("Output of array 2 :" + out1);
